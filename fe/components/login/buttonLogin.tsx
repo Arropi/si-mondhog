73dc76e4a5c371fe13a3176aab5c65e8ce4c7b7a
Matthew Priantara <matthewhayunajipriantara@mail.ugm.ac.id>
2026-03-05 15:35:37 +0700

    Feat: Auth Login

--- fe/components/login/buttonLogin.tsx
+++ fe/components/login/buttonLogin.tsx
@@ -1,11 +1,18 @@
 "use client"
 
+import { signIn } from "next-auth/react"
 import Image from "next/image"
+
+const handleLogin = async() => {
+    await signIn("google", {callbackUrl: '/dashboard'})
+}
 
 const ButtonLogin = () => {
     return (
         <>
-            <button className="flex items-center justify-center gap-3 px-6 sm:px-8 py-3 bg-white border border-gray-200 rounded-xl shadow-sm hover:shadow-md hover:bg-gray-50 transition-all font-semibold text-black w-lg cursor-pointer">
+            <button
+                onClick={handleLogin}
+                className="flex items-center justify-center gap-3 px-6 sm:px-8 py-3 bg-white border border-gray-200 rounded-xl shadow-sm hover:shadow-md hover:bg-gray-50 transition-all font-semibold text-black w-lg cursor-pointer">
                 <Image
                     src="/images/googleIcon.svg"
                     alt="Google Icon"
