5b0cc744500eaea21838f7734e51cf96d0fea194
Arropi <m.faizarrofi@gmail.com>
2026-03-07 13:11:31 +0700

    docs: clearify documentation of route auth

--- be/controllers/auth-controller.js
+++ be/controllers/auth-controller.js
@@ -1,8 +1,10 @@
 import { loginService } from "../services/auth-service.js";
-export async function login(req, res) {
+export async function login(req, res, next) {
     try {
         if (!req.body) {
-            return res.status(400).json({ message: "Bad Request" })
+            const error = new Error("Bad Request: No data provided");
+            error.statusCode = 400;
+            throw error;
         }
         const { username, email } = req.body
         const {status, ...result} = await loginService(username, email)
@@ -11,9 +13,6 @@ export async function login(req, res) {
             ...result
         })
     } catch (error) {
-        // console.log(error.name)
-        // console.error(error)
-        // res.status(500).json({ message: "Internal Server Error" })
         next(error)
     }
 }
