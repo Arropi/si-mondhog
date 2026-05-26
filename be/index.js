56c0e14d16f046ec63e570a07841c515c4b0e410
Arropi <m.faizarrofi@gmail.com>
2026-05-26 14:07:21 +0700

    fix: crashed vercel function

--- be/index.js
+++ be/index.js
@@ -16,8 +16,16 @@ import { PORT } from './config/env.js'
 const app = express()
 
 app.use(express.json())
-app.use(cors())
-app.options('*', cors())
+app.use(cors({
+  origin: [
+    process.env.FRONTEND_BASE_URL,
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}))
 await connectDB()
 
 app.get('/', (req, res) => {
